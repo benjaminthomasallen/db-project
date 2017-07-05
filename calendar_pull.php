8475cf88c6d092c613b25cee0ee14bd5c33394e9
Benjamin Allen <benthomasallen@gmail.com>
2017-07-04 22:24:06 -0400

    syntax errors fixed

--- calendar_pull.php
+++ calendar_pull.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "http://events.ucf.edu/upcoming/feed.xml"
+$url = "http://events.ucf.edu/upcoming/feed.xml";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -10,14 +10,14 @@ curl_close($ch);
 
 $xml = simplexml_load_string($data);
 
-$con = mysql_connect("localhost", "root", "");
+$con = mysql_connect("127.0.0.1", "root", "root");
 mysql_select_db("school_db", $con) or die(mysql_error());
 
 foreach($xml -> event as $row){
         $id = $row -> id;
         $title = $row -> title;
         $start_date = $row -> start_date;
-        $end_date = $row -> end_date:
+        $end_date = $row -> end_date;
         $location = $row -> location;
         $description = $row -> description;
         $contact_phone = $row -> contact_phone;
@@ -25,13 +25,13 @@ foreach($xml -> event as $row){
         $category = $row -> category;
 
 $sql = "INSERT INTO 'event' ('eid', 'rso_id', 'name', 'visibility', 'email', 'type', 'phone', 'start_date', 'end_date', 'location', 'description')"
-            "VALUES ('$id', '1', 'title', 1, '$contact_email', '$category', '$contact_email', '$start_date', '$end_date', '$location', '$description')";
+            . "VALUES ('$id', '1', '$title', '1', '$contact_email', '$category', '$contact_email', '$start_date', '$end_date', '$location', '$description')";
 
 $result = mysql_query($sql);
-if(!result){
+if (!$result){
     echo 'MySQL ERROR';
     } else {
     echo 'SUCCESS';
     }
-
+                            }
  ?>
