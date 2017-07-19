4275e8a8c103abcffd9606628ed755979fca4946
Benjamin Allen <benthomasallen@gmail.com>
2017-07-19 12:32:27 -0400

    added pull of owner name to rso page

--- php/populate_rso.php
+++ php/populate_rso.php
@@ -1,15 +1,24 @@
 <?php
 include 'functions.php';
 
-$sql = "SELECT rso_id, uid, name FROM rso";
+$sql = "SELECT
+            a.rso_id,
+            a.uid,
+            a.name,
+            b.first_name,
+            b.last_name
+        FROM
+            rso a
+                JOIN users b
+                    ON a.uid = b.uid";
 
 $result = queryMysql($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>RSO ID</th><th>User ID</th><th>Name</th></tr>";
+    echo "<table><tr><th>RSO ID</th><th>User ID</th><th>RSO Name</th><th>Owner Name</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["rso_id"]."</td><td>".$row["uid"]."</td><td>".$row["name"]."</td></tr>";
+        echo "<tr><td>".$row["rso_id"]."</td><td>".$row["uid"]."</td><td>".$row["name"]."</td><td>".$row["first_name"]." ".$row["last_name"]."</td></tr>";
     }
     echo "</table>";
 } else {
