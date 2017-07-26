3e584f40781784a69e2f61e23734f05a78d11929
Benjamin Allen <benthomasallen@gmail.com>
2017-07-26 13:53:03 -0400

    display comments on event pages

--- php/event_page.php
+++ php/event_page.php
@@ -32,6 +32,38 @@ if ($result->num_rows >0)
         echo "<tr><td class=indent>" . $row["location"] . " " . $row["room"] . "</td></tr>";
         echo "<tr><td class=indent>" . $row["description"] . "</td></tr>";
     }
+    echo "</table></br>";
+}
+
+$sql = "SELECT
+            a.uid,
+            a.first_name,
+            a.last_name,
+            b.uid,
+            b.comment,
+            b.rating
+        FROM users a
+        JOIN user_comments b
+            ON a.uid=b.uid
+        WHERE b.eid = $pull_eid";
+
+$result = queryMysql($sql);
+
+if ($result->num_rows > 0)
+{
+    echo "<strong>Comments: </strong></br><table>";
+    while($row = $result->fetch_assoc())
+    {
+        echo "<tr><td>" . $row['first_name'] . " " . $row['last_name'] ."</td></tr>";
+        if($row['rating'] == 0)
+        {
+            echo "<tr><td class='indent'> No rating given</td></tr>";
+        }
+        else {
+            echo "<tr><td class='indent'>" . "Rating: " . $row['rating'] . "/5</td></tr>";
+        }
+        echo "<tr><td class='indent'>Comment: " . $row['comment'] . "</td></tr>";
+    }
     echo "</table></br></body>";
 }
 
@@ -44,7 +76,7 @@ echo"<form method='post' action='post_comment.php'>
   <input type='radio' name='rating' value='1'> 1
   <input type='radio' name='rating' value='2'> 2
   <input type='radio' name='rating' value='3'> 3
-  <input type='radio' name='rating' value='4'> 4 
+  <input type='radio' name='rating' value='4'> 4
   <input type='radio' name='rating' value='5'> 5 </br>
 
   </br>
@@ -52,5 +84,6 @@ echo"<form method='post' action='post_comment.php'>
 
   <button> Submit comment </button><input type ='hidden' name='eid' value = '$pull_eid'/>";
 
+echo "</form></hmtl>";
 
 ?>
