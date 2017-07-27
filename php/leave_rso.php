5d85febadf1c3807426d9f915c5aeb72f6e46839
Fernando Soler-Casanova <fsoler58@knights.ucf.edu>
2017-07-27 02:43:32 -0400

    Removes the admin and transfers power to next on member list

--- php/leave_rso.php
+++ php/leave_rso.php
@@ -6,7 +6,7 @@
 
       $rsoid = $_POST['rsoid'];
 
-      $sql = "SELECT * FROM rso_member WHERE $rsoid = rso_id AND $uid = uid";
+      $sql = "SELECT * FROM rso_member WHERE rso_id= '$rsoid' AND uid= '$uid' ";
       $result = queryMysql($sql);
 
       if($result->num_rows)
@@ -15,6 +15,31 @@
                     WHERE rso_member.uid = '$uid' AND
                     rso_member.rso_id = '$rsoid'";
         queryMysql($sql);
+        // CHECK to See if they were an admin
+        $sql = "SELECT * FROM admin WHERE rso_id = $rsoid and uid= '$uid' ";
+        $result =queryMySql($sql);
+        if($result->num_rows)
+        {
+          // Pull all current members; Take the Top member UID
+          $sql = "SELECT uid FROM rso_member WHERE rso_id = '$rsoid' ";
+          $result = queryMysql($sql);
+          $row = $result->fetch_assoc();
+          $newAdmin = $row['uid'];
+
+          // Make them the new admin
+          $sql = "UPDATE admin "
+                ."SET uid = '$newAdmin' "
+                ."WHERE rso_id = '$rsoid' ";
+          queryMysql($sql);
+          // Update the RSO too
+          $sql = "UPDATE rso "
+                ."SET uid = '$newAdmin' "
+                ."WHERE rso_id = '$rsoid' ";
+          queryMysql($sql);
+          echo "<h4>Removed you as the Admin<h4>";
+
+        }
+
         die("<h4>Removed from RSO</h4><a href='populate_rso.php'>Return</a>");
       }
       else
